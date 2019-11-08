8118a59d1f6dc5371b2a919a2dda612d4322e188
robert.lee <robert0714@gmail.com>
2019-11-08 18:42:34 +0800

    adjust

--- Chapter12/labs/centos7-postgres-11-pglogical-lab/scripts/post-deploy.sh
+++ Chapter12/labs/centos7-postgres-11-pglogical-lab/scripts/post-deploy.sh
@@ -8,6 +8,15 @@ APP_DB_PASS=omnidb
 # Edit the following to change the name of the database that is created (defaults to the user name)
 APP_DB_NAME=omnidb_tests
 
+# Edit the following to change the version of PostgreSQL that is installed
+PG_VERSION=11
+
+# Edit the following to change the local port PostgreSQL port 5432 will be mapped to
+PG_LOCAL_PORT=5432
+
+PG_CONF="/var/lib/pgsql/$PG_VERSION/main/postgresql.conf"
+PG_HBA="/var/lib/pgsql/$PG_VERSION/main/pg_hba.conf"
+PG_DIR="/usr/pgsql-$PG_VERSION/bin"
 
 value=$( grep -ic "entry" /etc/hosts )
 if [ $value -eq 0 ]
@@ -41,13 +50,25 @@ sudo yum install -y postgresql11-pglogical
 # initializes database
 sudo  /usr/pgsql-11/bin/postgresql-11-setup initdb
 
+sudo echo "
+# Database administrative login by Unix domain socket
+local   all             postgres                                peer
 
-sudo sh -c 'echo local      replication     all                             trust  >>  /var/lib/pgsql/11/data/pg_hba.conf'
-sudo sh -c 'echo host       replication     all     127.0.0.1/32            trust  >>  /var/lib/pgsql/11/data/pg_hba.conf'
-sudo sh -c 'echo host       replication     all     ::1/128                 trust  >>  /var/lib/pgsql/11/data/pg_hba.conf'
-sudo sh -c 'echo host       replication     all     100.100.110.101/24      trust  >>  /var/lib/pgsql/11/data/pg_hba.conf'
-sudo sh -c 'echo host       replication     all     100.100.110.102/24      trust  >>  /var/lib/pgsql/11/data/pg_hba.conf'
-sudo sh -c 'echo host       all             all     all                     md5    >>  /var/lib/pgsql/11/data/pg_hba.conf'
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+
+# "local" is for Unix domain socket connections only
+local   all             all                                     peer
+# IPv4 local connections:
+host    all             all             127.0.0.1/32            md5
+# IPv6 local connections:
+host    all             all             ::1/128                 md5
+# Allow replication connections from localhost, by a user with the
+# replication privilege.
+local   replication     all                                     peer
+host    replication     all             127.0.0.1/32            md5
+host    replication     all             ::1/128                 md5
+host    all             all             all                     md5
+" > /var/lib/pgsql/11/data/pg_hba.conf
 
 # sudo sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/g' /var/lib/pgsql/11/data/pg_hba.conf
 sudo sh -c "echo listen_addresses = \'*\'   >>  /var/lib/pgsql/11/data/postgresql.conf"
@@ -56,6 +77,7 @@ sudo sh -c "echo max_worker_processes = 10   >>  /var/lib/pgsql/11/data/postgres
 sudo sh -c "echo max_replication_slots = 10   >>  /var/lib/pgsql/11/data/postgresql.conf"
 sudo sh -c "echo max_wal_senders = 10    >>  /var/lib/pgsql/11/data/postgresql.conf"
 sudo sh -c "echo shared_preload_libraries  = \'pglogical\'     >>  /var/lib/pgsql/11/data/postgresql.conf"
+sudo sh -c "echo client_encoding = utf8     >>  /var/lib/pgsql/11/data/postgresql.conf"
 
 sudo sh -c "echo max_wal_size = 20GB   >>  /var/lib/pgsql/11/data/postgresql.conf"
 sudo sh -c "echo checkpoint_timeout = 3600   >>  /var/lib/pgsql/11/data/postgresql.conf"
@@ -81,3 +103,35 @@ CREATE DATABASE $APP_DB_NAME WITH OWNER=$APP_DB_USER
                                   ENCODING='UTF8'
                                   TEMPLATE=template0;
 EOF
+
+
+###########################################################
+# Changes below this line are probably not necessary
+###########################################################
+print_db_usage () {
+  echo "Your PostgreSQL database has been setup and can be accessed on your local machine on the forwarded port (default: $PG_LOCAL_PORT)"
+  echo "  Host: localhost"
+  echo "  Port: $PG_LOCAL_PORT"
+  echo "  Database: $APP_DB_NAME"
+  echo "  Username: $APP_DB_USER"
+  echo "  Password: $APP_DB_PASS"
+  echo ""
+  echo "Admin access to postgres user via VM:"
+  echo "  vagrant ssh"
+  echo "  sudo su - postgres"
+  echo ""
+  echo "psql access to app database user via VM:"
+  echo "  vagrant ssh"
+  echo "  sudo su - postgres"
+  echo "  PGUSER=$APP_DB_USER PGPASSWORD=$APP_DB_PASS psql -h localhost $APP_DB_NAME"
+  echo ""
+  echo "Env variable for application development:"
+  echo "  DATABASE_URL=postgresql://$APP_DB_USER:$APP_DB_PASS@localhost:$PG_LOCAL_PORT/$APP_DB_NAME"
+  echo ""
+  echo "Local command to access the database via psql:"
+  echo "  PGUSER=$APP_DB_USER PGPASSWORD=$APP_DB_PASS psql -h localhost -p $PG_LOCAL_PORT $APP_DB_NAME"
+}
+
+echo "Successfully created PostgreSQL dev virtual machine."
+echo ""
+print_db_usage
